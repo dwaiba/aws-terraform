e8d4176e748e25a909fc365780181cc7b542d3a5
dwaiba <github@cloudgear.io>
2018-09-03 09:01:21 +0200

    e2e automated

--- main.tf
+++ main.tf
@@ -14,11 +14,34 @@ resource "aws_instance" "pegaweb" {
   tags {
     Name = "pega"
   }
+
   user_data = "${file("prep-rhel75.txt")}"
 }
 
 resource "aws_ebs_volume" "pegavol" {
   availability_zone = "us-west-2a"
   size              = 50
+}
+
+resource "null_resource" "provision" {
+  provisioner "remote-exec" {
+    connection {
+      user = "ec2-user"
+      host = "${aws_instance.pegaweb.public_dns}"
+
+      # The connection will use the local SSH agent for authentication.
+      private_key = "${file(var.private_key_path)}"
+    }
+
+    inline = [
+      "curl http://169.254.169.254/latest/user-data|sudo sh",
+    ]
+  }
+
+  depends_on = ["aws_instance.pegaweb", "aws_ebs_volume.pegavol", "aws_volume_attachment.ebs_att"]
+}
+
+output "address" {
+  value = "${aws_instance.pegaweb.public_dns}"
 }
 
--- variable.tf
+++ variable.tf
@@ -1,4 +1,8 @@
 variable "key_name" {
-  description = "Please input the key name in us-west2 region already present in your account"
+  description = "Please input the public key name in us-west2 region already present in your account and visible via ec2 console"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key - for ssh login. Example: ~/.ssh/terraform.pem"
 }
 
