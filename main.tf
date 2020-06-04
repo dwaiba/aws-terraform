9f611e84133faba78052c22cd9560c352cb26145
Dwai Banerjee <dwai@cloudgear.io>
2020-06-04 11:06:01 +0530

    added s3 with policies

--- main.tf
+++ main.tf
@@ -38,6 +38,20 @@ resource "aws_volume_attachment" "ebs_att" {
   force_detach = true
 }
 
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  attach_elb_log_delivery_policy = true
+  versioning = {
+    enabled = true
+  }
+}
 resource "aws_instance" "awsweb" {
   /**
                 ami = "${lookup(var.centosamis, var.region)}"
