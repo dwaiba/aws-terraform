b9fb136b19bf914b127320ca2793c014c451c8ca
Dwai Banerjee <dwai@cloudgear.io>
2020-06-08 17:06:53 +0530

    aadded dns support

--- main.tf
+++ main.tf
@@ -59,12 +59,13 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
   #enable_vpn_gateway   = true
   enable_dns_hostnames = true
   enable_dhcp_options  = true
-  enable_ipv6          = false
+  enable_dns_support   = true
+  enable_ipv6          = true
   tags = {
     Terraform                          = "true"
     Environment                        = "dev"
@@ -131,7 +132,8 @@ resource "null_resource" "provision" {
   }
 
   depends_on = [aws_instance.awsweb, aws_ebs_volume.awsvol, aws_volume_attachment.ebs_att]
-}
+ }
+
 resource "aws_elb" "bar" {
   name = "foobar-terraform-elb"
   #availability_zones = element(aws_instance.awsweb.*.availability_zone,count.index)
