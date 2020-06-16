5017b9099482f8a79e0a6ad262e335a767053d06
Dwai Banerjee <dwai@cloudgear.io>
2020-06-16 19:23:56 +0530

    added secondary cidr user_data for instance templates (via local-exec)

--- variable.tf
+++ variable.tf
@@ -78,7 +78,7 @@ variable "vpc_params" {
     "enable_dns_hostnames"   = true
     "enable_dhcp_options"    = true
     "enable_dns_support"     = true
-    "enable_ipv6"            = false
+    "enable_ipv6"            = true
     "environment_tag"        = "dev"
   }
 }
@@ -86,8 +86,9 @@ variable "vpc_params" {
 variable "vpc_subnets" {
   type = map
   default = {
-    "vpc_private_subnets" = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    "vpc_public_subnets"  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    "vpc_private_subnets"   = ["10.0.1.0/24", "10.1.2.0/24", "10.2.3.0/24"]
+    "vpc_public_subnets"    = ["10.0.101.0/24", "10.1.102.0/24", "10.2.103.0/24"]
+    "secondary_cidr_blocks" = ["10.1.0.0/16", "10.2.0.0/16"]
   }
 }
 
